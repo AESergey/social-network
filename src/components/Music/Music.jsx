e96990984ab35de1ce990850b36efc4af512a56b
AESergey <epsilovsergey@gmail.com>
2021-03-17 19:34:32 +0300

    adding Music, News, Setting components

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,12 @@
+import classes from './Music.module.css';
+import React, { Component } from 'react';
+
+const Music = (props) => {
+    return (
+        <div>
+            Music
+        </div>
+    );
+}
+
+export default Music;
