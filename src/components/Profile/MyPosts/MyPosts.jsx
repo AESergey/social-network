d4bda70a0047cc5816108551e7d4d6aee29592c4
AESergey <epsilovsergey@gmail.com>
2021-03-20 10:59:00 +0300

    Добавил компонент MyPosts

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,41 @@
+import classes from './MyPosts.module.css';
+import React, { Component } from 'react';
+import Post from './Post/Post';
+
+
+
+
+const MyPosts = (props) => {
+
+    let postsElements = props.posts.map((el) => <Post message={el.message} likeCount={el.likeCount} />);
+
+    let newPostElement = React.createRef();
+
+    let onAddPost = () => {
+        props.addPost();
+    }
+
+    let onPostChange = () => {
+        let text = newPostElement.current.value;
+        props.updateNewPostText(text);
+    }
+
+    return (
+        <div className={classes.postsBlock}>
+            <h3>My posts</h3>
+            <div>
+                <div>
+                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
+                </div>
+                <div>
+                    <button onClick={onAddPost}>Add post</button>
+                </div>
+            </div>
+            <div className={classes.posts}>
+                {postsElements}
+            </div>
+        </div>
+    );
+}
+
+export default MyPosts;
